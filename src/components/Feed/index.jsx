984d9daf6cfba95d975650349791271a573d1d7a
VoTan <vdt040499@gmail.com>
2020-09-10 19:46:24 +0700

    MessageSender

--- src/components/Feed/index.jsx
+++ src/components/Feed/index.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './Feed.css';
 import StoryReel from '../StoryReel';
+import MessageSender from '../MessageSender';
 function Feed() {
     return (
         <div className="feed">
             <StoryReel />
-            {/* MessageSender */}
+            <MessageSender />
         </div>
     )
 }
