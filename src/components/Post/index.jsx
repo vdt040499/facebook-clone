7b492129bee13ad26e99c3a181aa3e8015dc75c3
VoTan <vdt040499@gmail.com>
2020-09-12 09:49:12 +0700

    Render Posts

--- src/components/Post/index.jsx
+++ src/components/Post/index.jsx
@@ -9,7 +9,7 @@ import { ExpandMoreOutlined } from '@material-ui/icons';
 import { Avatar } from '@material-ui/core';
 
 function Post(props) {
-    let { profilePic, image, username, message } = props;
+    let { profilePic, image, username, timestamp, message } = props;
 
     return (
         <div className="post">
@@ -17,7 +17,7 @@ function Post(props) {
                 <Avatar className="post__avatar" src={profilePic}/>
                 <div className="post__topInfo">
                     <h3>{username}</h3>
-                    <p>Timestamp...</p>
+                    <p>{new Date(timestamp?.toDate()).toUTCString()}</p>
                 </div>
             </div>
             <div className="post__bottom">
