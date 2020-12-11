89b1fa1e834e435b7768a8559e63b0e3d8105675
Vo Tan <vdt040499@gmail.com>
2020-12-11 12:49:32 +0700

    Update image on StoryReel

--- src/components/StoryReel/index.jsx
+++ src/components/StoryReel/index.jsx
@@ -1,37 +1,37 @@
-import React from 'react'
-import Story from '../Story'
-import './StoryReel.css';
+import React from "react";
+import Story from "../Story";
+import "./StoryReel.css";
 
 function StoryReel() {
-    return (
-        <div className="storyreel">
-            <Story 
-                image="https://i.pinimg.com/originals/19/3b/ff/193bff606ef659249253cb7a84f10b3c.gif" 
-                profileSrc="https://scontent-sin6-2.xx.fbcdn.net/v/t1.0-9/118732314_2757711467834121_3580985112925782279_n.jpg?_nc_cat=103&_nc_sid=09cbfe&_nc_ohc=HED7bpilSd0AX9suTuL&_nc_ht=scontent-sin6-2.xx&oh=5a3104f369650e8b3384b1c1ccbae686&oe=5F800FF4"
-                title="Võ Tân"
-            />
-            <Story 
-                image="https://scontent-xsp1-1.xx.fbcdn.net/v/t1.15752-9/103903537_723661918449685_3249607228386237892_n.jpg?_nc_cat=108&_nc_sid=b96e70&_nc_ohc=I88T2hH0H5cAX8X0TJB&_nc_ht=scontent-xsp1-1.xx&oh=794ec11b895fb284419320fa91435ee4&oe=5F7D9AE1" 
-                profileSrc="https://scontent-xsp1-2.xx.fbcdn.net/v/t1.15752-9/51573454_652071328560087_1536418859588780032_n.png?_nc_cat=107&_nc_sid=b96e70&_nc_ohc=STCEW7gZyQ4AX_9AcuD&_nc_ht=scontent-xsp1-2.xx&oh=8a695aaa223a3e050f3b644fc4ff626d&oe=5F7E03EA"
-                title="Lâm Ngọc"
-            />
-            <Story 
-                image="https://scontent-xsp1-1.xx.fbcdn.net/v/t1.15752-9/116702259_771451450061189_5719183898093459819_n.png?_nc_cat=105&_nc_sid=b96e70&_nc_ohc=e-qx9Xpf8zIAX8xcRkB&_nc_ht=scontent-xsp1-1.xx&oh=c2d0faf31d82bee759a62a401a7e3b4d&oe=5F7F831A" 
-                profileSrc="https://scontent-sin6-2.xx.fbcdn.net/v/t1.0-9/92952015_2528071837407367_3358201329213243392_n.jpg?_nc_cat=108&_nc_sid=174925&_nc_ohc=qVgoPZrZURcAX_WOThl&_nc_ht=scontent-sin6-2.xx&oh=0a615aa395041b724c1121336a6dad16&oe=5F7CEFEA"
-                title="Oanh Hoàng"
-            />
-            <Story 
-                image="https://scontent-xsp1-2.xx.fbcdn.net/v/t1.15752-9/s2048x2048/118731461_329107068439439_1726030223790338393_n.jpg?_nc_cat=107&_nc_sid=b96e70&_nc_ohc=r4RYQ8uoRB8AX--m4hP&_nc_ht=scontent-xsp1-2.xx&tp=7&oh=1cd5f649ab38a719866150574cbc40b7&oe=5F7DCF03" 
-                profileSrc="https://scontent-xsp1-2.xx.fbcdn.net/v/t1.15752-9/81812317_2715065155255047_5655861248907018240_n.png?_nc_cat=104&_nc_sid=b96e70&_nc_ohc=o7lVoKuIaU8AX8XWEMu&_nc_ht=scontent-xsp1-2.xx&oh=84b0e3757c9b980caf6a67425735857a&oe=5F7C9D62"
-                title="Ngọc Phương"
-            />
-            <Story 
-                image="https://scontent-xsp1-1.xx.fbcdn.net/v/t1.15752-9/118703078_333855314694164_4285668578721035321_n.jpg?_nc_cat=108&_nc_sid=b96e70&_nc_ohc=-zJFWpQf0DIAX_BqHQF&_nc_ht=scontent-xsp1-1.xx&oh=be6606c911bc181290e81a20223b1bb7&oe=5F7EC531" 
-                profileSrc="https://scontent-xsp1-2.xx.fbcdn.net/v/t1.15752-9/83630643_2413252658988289_6386472487747584000_n.png?_nc_cat=101&_nc_sid=b96e70&_nc_ohc=DhHFfUlhNjEAX_FCqPd&_nc_ht=scontent-xsp1-2.xx&oh=e7e0ac8abc7166afb030a2e1e4bebb59&oe=5F7E7223"
-                title="Tống Ngọc"
-            />
-        </div>
-    )
+  return (
+    <div className="storyreel">
+      <Story
+        image="https://i.pinimg.com/736x/b6/5e/23/b65e235b49f98e428d500feb6664680b.jpg"
+        profileSrc="https://png.pngtree.com/png-clipart/20190517/original/pngtree-staying-cute-line-firefly-illustration-png-image_4087336.jpg"
+        title="Đóm"
+      />
+      <Story
+        image="https://i.pinimg.com/736x/15/fa/33/15fa33700cefbdd7ee7f2b2a4018a5e0.jpg"
+        profileSrc="https://i.pinimg.com/736x/c2/a5/2d/c2a52d77efcc0119386241e9b7e2ed9b.jpg"
+        title="Trịnh Tổng"
+      />
+      <Story
+        image="https://i.pinimg.com/originals/aa/4a/26/aa4a2600db1ae6e4a278d52450dca6db.jpg"
+        profileSrc="https://vcdn-giaitri.vnecdn.net/2020/12/06/Jack1200-2-4518-1605518144-7661-1607259645.jpg"
+        title="Đóm Chúa"
+      />
+      <Story
+        image="https://i.pinimg.com/originals/47/b3/2c/47b32c87039744ad6a610e6c31a453d3.jpg"
+        profileSrc="https://cuoifly.tuoitre.vn/1200/900/ttc/r/2020/11/07/124029837-1491303771257676-1340519486941101085-n-1604712860-16x9.jpg"
+        title="Meomeo"
+      />
+      <Story
+        image="https://i.pinimg.com/originals/f9/df/2e/f9df2e5dcbeeb4adc22351918051291a.jpg"
+        profileSrc="https://cuoifly.tuoitre.vn/820/0/ttc/r/2020/03/07/87493949-888348224961641-1925515705168429056-o-1583543907.jpg"
+        title="J97"
+      />
+    </div>
+  );
 }
 
-export default StoryReel
+export default StoryReel;
